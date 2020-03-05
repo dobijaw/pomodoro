b2970b18df439cd36eea2eae605f70805e3c48b7
Wojciech Dobija <dobijaw@gmail.com>
2020-03-05 01:55:21 +0100

    add some styles

---
+++ src/components/atoms/CloseButton/CloseButton.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  display: block;
+  width: auto;
+  height: 30px;
+  border: 0;
+  background: none;
+`;
+
+const ButtonItem = styled.span`
+  display: block;
+  width: 15px;
+  height: 2px;
+  background: ${({ theme }) => theme.colors.buttonsAndLinks};
+
+  &:first-child {
+    transform: translateY(1px) rotate(45deg);
+  }
+
+  &:last-child {
+    transform: translateY(-1px) rotate(-45deg);
+  }
+`;
+
+const CloseButton = ({ handleClick }) => (
+  <Button type="button" onClick={handleClick}>
+    <ButtonItem />
+    <ButtonItem />
+  </Button>
+);
+
+export default CloseButton;
