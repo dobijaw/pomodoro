f938d38c7f7d6e187bf529c2e0c8bc868fdaf4a4
Wojciech Dobija <dobijaw@gmail.com>
2020-03-07 13:09:53 +0100

    add settings template

--- src/components/molecules/FormItem/FormItem.js
+++ src/components/molecules/FormItem/FormItem.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Input from 'components/atoms/Input/Input';
 import Label from 'components/atoms/Label/Label';
 
 const StyledWrapper = styled.div`
-  max-width: ${({ width }) => width || '400px'};
+  width: 100%;
   padding: 10px 0 10px 0;
   border-bottom: 1px solid ${({ theme }) => theme.colors.details};
   margin: 20px 0;
 `;
 
-const FormItem = ({ width, label }) => (
-  <StyledWrapper width={width}>
+const FormItem = ({ label, type }) => (
+  <StyledWrapper>
     <Label>{label}</Label>
-    <Input placeholder="e.g. 5:00" />
+    <Input placeholder="e.g. 5:00" type={type} />
   </StyledWrapper>
 );
+
+FormItem.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+};
+
+FormItem.defaultProps = {
+  type: 'text',
+};
 
 export default FormItem;
--- src/templates/SettingsTemplate.js
+++ src/templates/SettingsTemplate.js
@@ -2,32 +2,72 @@ import React from 'react';
 import styled from 'styled-components';
 import Headline from 'components/atoms/Headline/Headline';
 import FormCooseItem from 'components/molecules/FormChooseItem/FormChooseItem';
+import FormItem from 'components/molecules/FormItem/FormItem';
 
 const SettingsWrapper = styled.div`
-  background: red;
+  margin-top: 10px;
 `;
 
 const StyledInnerWrapper = styled.section`
   margin-bottom: 60px;
-  background: blue;
+`;
+
+const StyledThemeWrapper = styled.div`
+  display: flex;
+
+  > div {
+    margin-right: 60px;
+  }
+`;
+
+const StyledSessionWrapper = styled.div`
+  display: flex;
+  > div {
+    &:first-of-type {
+      margin-right: 20px;
+    }
+
+    &:last-of-type {
+      margin-left: 20px;
+    }
+  }
+`;
+
+const StyledDataWrapper = styled.div`
+  display: flex;
 `;
 
 const SettingsTemplate = () => (
   <SettingsWrapper>
     <StyledInnerWrapper>
       <Headline as="h2">Pomodoro theme</Headline>
-      <FormCooseItem name="themeColor" type="radio" id="themeColorDark">
-        Dark color
-      </FormCooseItem>
-      <FormCooseItem name="themeColor" type="radio" id="themeColorLight">
-        Light color
-      </FormCooseItem>
+      <StyledThemeWrapper>
+        <FormCooseItem name="themeColor" type="radio" id="themeColorDark">
+          Dark color
+        </FormCooseItem>
+        <FormCooseItem name="themeColor" type="radio" id="themeColorLight">
+          Light color
+        </FormCooseItem>
+      </StyledThemeWrapper>
     </StyledInnerWrapper>
     <StyledInnerWrapper>
       <Headline as="h2">Default session</Headline>
+      <StyledSessionWrapper>
+        <FormItem label="session time" />
+        <FormItem label="break time" />
+      </StyledSessionWrapper>
     </StyledInnerWrapper>
     <StyledInnerWrapper>
       <Headline as="h2">Your data</Headline>
+      <StyledDataWrapper>
+        <FormItem label="name" />
+        <FormItem label="email" />
+      </StyledDataWrapper>
+      <StyledDataWrapper>
+        <FormItem label="old password" type="password" />
+        <FormItem label="new password" type="password" />
+        <FormItem label="repeat new password" type="password" />
+      </StyledDataWrapper>
     </StyledInnerWrapper>
   </SettingsWrapper>
 );
