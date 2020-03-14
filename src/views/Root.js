cbc1c493577c913499ec75f2179a247e6bc1ade4
Wojciech Dobija <dobijaw@gmail.com>
2020-03-14 19:55:11 +0100

    rebuild root, create App component with Redux Provider

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { routes } from 'routes/index';
-import { Provider } from 'react-redux';
-import store from 'store';
 import MainTemplate from 'templates/MainTemplate';
 import TimerPage from 'views/TimerPage';
 import CycleModal from 'views/CycleModal';
@@ -13,44 +11,35 @@ import ProjectsPage from 'views/ProjectsPage';
 const Root = () => {
   // eslint-disable-next-line
   const [isModalVisible, setModalVisible] = useState(false);
+  const [counter, setCounter] = useState(1);
 
-  const handleToggleModalVisible = () => {
-    setModalVisible(!isModalVisible);
-  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log(counter);
+      setCounter(counter + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [counter, setCounter]);
 
   const handleCloseModal = () => {
     setModalVisible(false);
   };
 
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <MainTemplate>
-          <>
-            <Switch>
-              <Route
-                exact
-                path={routes.timer}
-                component={() => <TimerPage handleModalButtonClick={handleToggleModalVisible} />}
-              />
-              <Route
-                path={routes.settings}
-                component={() => <SettingsPage handleModalButtonClick={handleToggleModalVisible} />}
-              />
-              <Route
-                path={routes.history}
-                component={() => <HistoryPage handleModalButtonClick={handleToggleModalVisible} />}
-              />
-              <Route
-                path={routes.projects}
-                component={() => <ProjectsPage handleModalButtonClick={handleToggleModalVisible} />}
-              />
-            </Switch>
-            {isModalVisible && <CycleModal handleCloseModal={handleCloseModal} />}
-          </>
-        </MainTemplate>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <MainTemplate>
+        <>
+          <Switch>
+            <Route exact path={routes.timer} component={TimerPage} />
+            <Route path={routes.settings} component={SettingsPage} />
+            <Route path={routes.history} component={HistoryPage} />
+            <Route path={routes.projects} component={ProjectsPage} />
+          </Switch>
+          {isModalVisible && <CycleModal handleCloseModal={handleCloseModal} />}
+        </>
+      </MainTemplate>
+    </BrowserRouter>
   );
 };
 
