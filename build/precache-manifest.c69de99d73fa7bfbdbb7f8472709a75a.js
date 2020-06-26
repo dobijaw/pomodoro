50a15b53677ae3485d3c5f41fc77108814a6d96b
Wojciech Dobija <dobijaw@gmail.com>
2020-06-26 16:40:57 +0200

    build app

---
+++ build/precache-manifest.c69de99d73fa7bfbdbb7f8472709a75a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011a7605fb9ceebbe76190eacdb54ebb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "883a3d541135f72637da",
+    "url": "/static/js/2.b977b657.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b977b657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e79164a11218fce389",
+    "url": "/static/js/main.60453d72.chunk.js"
+  },
+  {
+    "revision": "343bc61d20d623f43a65",
+    "url": "/static/js/runtime-main.6b805be8.js"
+  }
+]);
